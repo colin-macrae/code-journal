65b967b23ac5fde26417222a35d016cc1a1f8994
Colin Macrae <csmacrae@gmail.com>
2023-02-14 18:59:54 +0000

    stores the form's input values in a new object #1

--- js/data.js
+++ js/data.js
@@ -6,3 +6,24 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+// var newObj = [
+//   {
+//     title: '',
+//     url: '',
+//     notes: '',
+//     entryId: 0
+//   }
+// ];
+
+// newObj.title = 'Ada';
+// newObj.entryId = data.nextEntryId;
+// console.log(newObj);
+
+// newObj[1].title = 'hiThere';
+// console.log(newObj[1]);
+
+// newObj.push(data);
+// console.log(newObj);
+
+// data.push(newObj);
--- js/main.js
+++ js/main.js
@@ -1,15 +1,67 @@
 var $photoUrl = document.querySelector('.photo-url');
-$photoUrl.addEventListener('input', doStuff);
-function doStuff(e) {
-  var image = document.querySelector('.image');
-  image.src = e.target.value;
-  // console.log(e.target.value);
+$photoUrl.addEventListener('input', renderUrl);
+function renderUrl(e) {
+  var $image = document.querySelector('.image');
+  $image.src = e.target.value;
 }
+
+var $save = document.querySelector('form');
+$save.addEventListener('submit', storeInput);
+
+function storeInput(e) {
+  e.preventDefault();
+  // console.log(e.object);
+  // console.log('submitted');
+  // var $newObj = [
+  //   {
+  //     title: '',
+  //     url: '',
+  //     notes: '',
+  //     entryId: 0
+  //   }
+  // ];
+  // $newObj.url = $photoUrl.value;
+}
+
+// if the submit button is hit, at that moment take the values in the three input areas and save them to the object
+// how do you get those values? it seems i need to querySelect all of them,
+
+// if (e.target === save) {
+//   var title = document.querySelector('.title');
+//   title.addEventListener('submit', storeTitle);
+// }
+// function storeTitle(e) {
+//   newObj.title += e.target.value;
+// }
+
+// save.addEventListener('click', function(e) {console.log('clicked')});
+
+// var title = document.querySelector('.title');
+// title.addEventListener('submit', storeTitle);
+// function storeTitle(e) {
+//   console.log('title submitted');
+// }
+// // newObj.title = e.target.value;
 
 // the next step is to get the url and set the src attribute to that photo.  how to do that?
 // you need a method or something to get it
 // then you the same to set it
 // what is the method/methods for that?
+
+// var save = document.querySelector('.save-button');
+// // save.addEventListener('click', function(e) {console.log('clicked')});
+// save.addEventListener('submit', storeInput);
+// function storeInput(e) {
+//   e.preventDefault();
+//   var title = document.querySelector('.title');
+//   title.addEventListener('submit', storeTitle);
+//   function storeTitle(e) {
+//     console.log('title submitted');
+//     // newObj.title += e.target.value;
+//     // console.log(newObj);
+//   }
+//   // newObj.title = e.target.value;
+// }
 
 // var $image = document.querySelector('.image');
 // $image.HTMLImageElement.src = e.target.value
