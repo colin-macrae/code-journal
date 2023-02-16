581e82b3c7c47d22afdaff53416dedb07c2b610f
Colin Macrae <csmacrae@gmail.com>
2023-02-16 00:15:52 +0000

    Remove the example HTML entries from index.html once the user's entries are successfully being appended to the DOM on page load and replace them with a message indicating that no entries have been recorded which matches the figma. #1

--- js/main.js
+++ js/main.js
@@ -20,4 +20,50 @@ function storeInput(e) {
   var $image = document.querySelector('.image');
   $image.src = 'images/placeholder-image-square.jpg';
   $save.reset();
+  renderEntry();
 }
+
+// BEGIN ISSUE 2 user-can-view-their-entries
+function renderEntry(entry) {
+  var entryDomTree = document.createElement('li');
+  entryDomTree.setAttribute('class', 'entries');
+  // console.log(entryDomTree);
+
+  var entryImage = document.createElement('img');
+  entryImage.setAttribute('src', entry.url);
+  // console.log(entryImage);
+
+  var entryTitleAndNotesDiv = document.createElement('div');
+  entryTitleAndNotesDiv.setAttribute('class', 'title-and-notes');
+  // console.log(entryTitleAndNotesDiv);
+
+  var entryTitle = document.createElement('h2');
+  entryTitle.setAttribute('class', 'entry-title');
+  entryTitle.textContent = entry.title;
+  // console.log(entryTitle);
+
+  var entryNotes = document.createElement('p');
+  entryNotes.setAttribute('class', 'entry-notes');
+  entryNotes.textContent = entry.notes;
+
+  // console.log(entryNotes);
+
+  entryDomTree.appendChild(entryImage);
+  entryDomTree.appendChild(entryTitleAndNotesDiv);
+  entryTitleAndNotesDiv.appendChild(entryTitle);
+  entryTitleAndNotesDiv.appendChild(entryNotes);
+  return entryDomTree;
+
+}
+// renderEntry(data.entries[0]);
+
+var attachEntryDomTree = document.querySelector('.ul-no-bullets');
+
+document.addEventListener('DOMContentLoaded', loopEntriesAndCreateDom);
+function loopEntriesAndCreateDom(e) {
+  for (var i = 0; i < data.entries.length; i++) {
+    var singleEntryTree = renderEntry(data.entries[i]);
+    attachEntryDomTree.appendChild(singleEntryTree);
+  }
+}
+// console.log(renderEntry(entry));
